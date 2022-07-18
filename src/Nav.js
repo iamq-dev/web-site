eb161a844b0aabf119d865dbdfca79629d99af37
iamq-dev <q@iamq.dev>
2022-07-17 19:44:58 -0500

    Updates

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => (
+  <nav>
+    <h1>Logo</h1>
+    <ul>
+      <li>Hoagedon
+        me</li>
+      <li>not home</li>
+      <li>test 2 </li>
+      <li>derp</li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
